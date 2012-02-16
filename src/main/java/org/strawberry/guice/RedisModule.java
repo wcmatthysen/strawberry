3bd63884561009928e71667d39b2eb7e8e2aec34
Wiehann Matthysen <wcmatthysen@gmail.com>
2012-02-16 23:48:00 +0200

    Updated license information.
    Specified the library name and copyright information inside the license
    header. Also added the start and end-year to the copyright.

--- src/main/java/org/strawberry/guice/RedisModule.java
+++ src/main/java/org/strawberry/guice/RedisModule.java
@@ -1,4 +1,7 @@
 /**
+ * Strawberry Library
+ * Copyright (C) 2011 - 2012
+ *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
