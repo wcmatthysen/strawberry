2d0d414ada06034598137453ba7e2f54a5df9444
Wiehann Matthysen <wcmatthysen@gmail.com>
2012-02-29 21:09:23 +0200

    Added JavaDoc for RedisModule.

--- src/main/java/org/strawberry/guice/RedisModule.java
+++ src/main/java/org/strawberry/guice/RedisModule.java
@@ -22,8 +22,10 @@ import java.lang.reflect.Field;
 import org.strawberry.redis.RedisLoader;
 
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.inject.AbstractModule;
+import com.google.inject.Module;
 import com.google.inject.matcher.Matchers;
 
 import redis.clients.jedis.JedisPool;
@@ -31,18 +33,91 @@ import redis.clients.jedis.JedisPool;
 import fj.data.Option;
 
 /**
- *
+ * {@code RedisModule} is responsible for setting up the custom injections that
+ * need to occur for the {@link Redis} field-annotations that may be present in
+ * your classes. You simply need to install this {@link Module} inside your main
+ * Guice {@code Module}, giving it the {@link JedisPool} of connections to your
+ * Redis database as constructor argument. This is illustrated with the
+ * following example:
+ * 
+ * <pre>
+ * public class MyCustomModule extends AbstractModule {
+ *   
+ *   private final JedisPool pool = new JedisPool("localhost", 6379);
+ * 
+ *   {@literal @}Override
+ *   protected void configure() {
+ *     install(new RedisModule(this.pool));
+ *     // set up the rest of your bindings.
+ *     ...
+ *   }
+ * }
+ * </pre>
+ * 
+ * However, this assumes that you don't want any caching to occur for the field
+ * values that are loaded from the Redis database. For more fine-grained control
+ * you can manually provide the {@link LoadingCache} of {@link Field}-to-value
+ * mappings as the following example illustrates:
+ * 
+ * <pre>
+ * public class MyCustomModule extends AbstractModule {
+ *   
+ *   private final JedisPool pool = new JedisPool("localhost", 6379);
+ * 
+ *   {@literal @}Override
+ *   protected void configure() {
+ *     LoadingCache<Field, Option> cache =
+ *       CacheBuilder.newBuilder().
+ *       expireAfterWrite(1, TimeUnit.DAYS).
+ *       maximumSize(1000).
+ *       build(new RedisLoader(this.pool));
+ *     install(new RedisModule(cache));
+ *     // set up the rest of your bindings.
+ *     ...
+ *   }
+ * }
+ * </pre>
+ * 
+ * The abovementioned example will create a cache that stores up to 1000 field
+ * values, and will expire these values after a day before loading them again
+ * from the Redis database. See Google Guava's {@link CacheBuilder} class for
+ * more comprehensive examples on how to construct a customized cache.
+ * <b>Note</b>: the {@code CacheBuilder} requires a {@link CacheLoader} to
+ * actually load the values into the cache if no matching key-value pair could
+ * be found. The class responsible for this is {@link RedisLoader} and contains
+ * the bulk of the logic that pertains to loading and transforming values from
+ * Redis to be used as field-values.
+ * 
  * @author Wiehann Matthysen
  */
 public final class RedisModule extends AbstractModule {
 
     private final LoadingCache<Field, Option> cache;
 
+    /**
+     * Initializes a newly created {@code RedisModule} with the given cache of
+     * {@code Field}-to-value mappings.
+     * @param cache The cache that will serve as storage for field values that
+     * are loaded from the Redis database.
+     */
     public RedisModule(LoadingCache<Field, Option> cache) {
         this.cache = cache;
     }
 
+    /**
+     * Initializes a newly created {@code RedisModule} with the given
+     * {@code JedisPool} of connections to a Redis database. 
+     * @param pool The pool of connections to a Redis database.
+     */
     public RedisModule(JedisPool pool) {
+        
+        // This constructor would be called in situations where caching of
+        // Redis field-values are not important, and the latest most up-to-date
+        // values should be retrieved from Redis whevever the Injector creates
+        // an object with Redis-annotated fields.
+        
+        // It achieves this by creating a cache that never stores it's values
+        // (maximum size of 0).
         this.cache = CacheBuilder.newBuilder().maximumSize(0).build(new RedisLoader(pool));
     }
 
